7706ba6e3348fdeb31aacc51294b69c93de1f71d
dongjian <dongjian@easypay.com.cn>
2024-06-11 17:10:15 +0800

    feat(mybatis-demo):
    增加编程式事务（未测试）

--- dj-springboot-demo/mybatis-demo/src/main/java/dj/com/mybatis/service/impl/UserServiceImpl.java
+++ dj-springboot-demo/mybatis-demo/src/main/java/dj/com/mybatis/service/impl/UserServiceImpl.java
@@ -10,6 +10,11 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -22,6 +27,10 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements IUserService {
 
+
+    @Resource
+    PlatformTransactionManager transactionManager;
+
     @Resource
     UserDao userDao;
 
@@ -32,4 +41,29 @@ public class UserServiceImpl implements IUserService {
     public String selectAllAndDeal() {
         return "over";
     }
+
+    /**
+     * 编程式事务：https://blog.csdn.net/weixin_45433031/article/details/134575933
+     * 为什么使用编程式事务：https://www.yuque.com/hollis666/hgtuok/sscz8razzyxltzhe
+     */
+    public boolean testTransaction(){
+        TransactionStatus status;
+        // 手动开启事务初始化
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            //操作
+
+            // 操作无异常：提交事务
+            transactionManager.commit(status);
+            log.debug("操作xxxx成功");
+            return true;
+        } catch (Exception e) {
+            log.debug("操作xxxx成功出错,正在回滚,错误信息为:"+e.getMessage());
+            // 捕获异常, 事务回滚
+            transactionManager.rollback(status);
+            log.debug("操作xxxx已回滚");
+            return false;
+        }
+    }
+
 }
