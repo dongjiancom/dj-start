911eaf4d92b50e31ba956719847d90eab55a64e4
dongjian <dongjian@easypay.com.cn>
2024-06-25 17:28:09 +0800

    perf: 异步处理方式不用线程池

--- dj-springboot-demo/spring-async/src/main/java/com/feizhou/async/controller/DjAsyncController.java
+++ dj-springboot-demo/spring-async/src/main/java/com/feizhou/async/controller/DjAsyncController.java
@@ -1,5 +1,6 @@
 package com.feizhou.async.controller;
 
+import cn.hutool.core.thread.ThreadUtil;
 import com.feizhou.async.service.DjAsyncService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -9,21 +10,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
 
 /**
  * @author jiandong 2024-05-21 create
  */
 @RestController
 @Slf4j
-public class DjAsyncController {
+public class DjAsyncController implements Runnable{
 
     @Resource
     DjAsyncService djAsyncService;
 
+    @SneakyThrows
+    @PostMapping("/asyncSanduo")
+    @ResponseBody
+    public String asyncSanduo() {
+        List<Runnable> tasks = new ArrayList<>();
+        for (int i=0;i<1000;i++){
+            tasks.add(() -> this.run());
+        }
+
+        ExecutorService executorService = ThreadUtil.newExecutor(tasks.size());
+        tasks.stream().forEach(executorService::execute);
+        executorService.shutdown();
+
+        return "success";
+    }
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        TimeUnit.SECONDS.sleep(3l);
+        // 在这里添加你的逻辑
+        log.info("Runnable is running!");
+    }
 
     /**
      * async
