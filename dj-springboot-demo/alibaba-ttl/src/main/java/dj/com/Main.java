9b0e25fe32b7796dc6c571bc05f0f9282fc900fb
dongjian <dongjian@easypay.com.cn>
2024-07-22 11:23:22 +0800

    refactor

--- dj-springboot-demo/alibaba-ttl/src/main/java/dj/com/Main.java
+++ dj-springboot-demo/alibaba-ttl/src/main/java/dj/com/Main.java
@@ -1,7 +1,13 @@
 package dj.com;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.thread.ThreadUtil;
 import com.alibaba.ttl.TransmittableThreadLocal;
+import lombok.SneakyThrows;
 import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * hollis:
@@ -11,24 +17,25 @@ import org.junit.Test;
  * api:
  *  https://alibaba.github.io/transmittable-thread-local/apidocs/2.14.5/index.html
  */
-public class Main {
+public class Main implements Runnable{
+
+    static TransmittableThreadLocal<String> context = new TransmittableThreadLocal<>();
 
     /**
      * 1. 简单使用
      *      父线程给子线程传递值。
      */
+    @SneakyThrows
     @Test
     public void t1() {
-        TransmittableThreadLocal<String> context = new TransmittableThreadLocal<>();
         // 在父线程中设置
         context.set("value-set-in-parent");
 
-        // 新建一个线程
+        ExecutorService executorService = ThreadUtil.newExecutor(2);
+        executorService.submit(this);
 
         // 在子线程中可以读取，值是"value-set-in-parent"
-        String value = context.get();
-
-        System.out.println(value);
+        ThreadUtil.getMainThread().join();
     }
 
     /**
@@ -40,4 +47,12 @@ public class Main {
 
     }
 
+    @Override
+    public void run() {
+        Console.log("run - start");
+        System.out.println("run" + ThreadUtil.getMainThread().getId());
+        System.out.println(context.get());
+        ThreadUtil.sleep(1000l);
+        Console.log("run - stop");
+    }
 }
